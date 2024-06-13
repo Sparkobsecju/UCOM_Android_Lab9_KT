d5b5e5704a9b5efe498ea2b02b1ee01bc9c0dd62
Sivan Chen <ryan1825114@gmail.com>
2024-06-13 16:34:24 +0800

    add function of Writing something to SharedPreference

--- app/src/main/java/com/example/lab9_kt/MainActivity.kt
+++ app/src/main/java/com/example/lab9_kt/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.lab9_kt
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val FILE_NAME = "main_settings"
+        const val KEY1 = "greeting"
+    }
+
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+        editText = findViewById(R.id.editText)
+        button1.setOnClickListener { writeToSharedPreference() }
+    }
+
+    private fun writeToSharedPreference() {
+        val preferences = getSharedPreferences(FILE_NAME, 0)
+        val edit = preferences.edit()
+        edit.putString(KEY1, editText.text.toString())
+        edit.apply()
+        Toast.makeText(this, "shared preference 寫入成功", Toast.LENGTH_SHORT).show()
     }
 }
